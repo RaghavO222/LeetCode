7470feadaea182ccebb5b3751b4f93d5e1b11e38
Raghav <105598621+RaghavO222@users.noreply.github.com>
2026-09-03 18:03:44 +0530

    Implement mergeTwoLists for merging sorted lists

---
+++ 034. 2-Merge-Two-Sorted-Lists/Solutioin.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(); // Dummy node
+        ListNode curr = head;
+
+        while(list1 != null && list2 != null){
+            if(list1.val > list2.val){
+                curr.next = list2;
+                list2 = list2.next;
+            } else {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            curr = curr.next;
+        }
+        
+        // Attach the remaining nodes of whichever list is not empty
+        curr.next = (list1 != null) ? list1 : list2;
+
+        // Return the actual head, skipping the dummy node
+        return head.next;
+    }
+}
